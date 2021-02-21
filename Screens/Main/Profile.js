5e1d7e41c3445d9c2c1f819a452aaa8d3696f7fa
Dogu <dogudeniz.ugur@gmail.com>
2021-02-21 21:07:09 +0300

    badges with texts implemented

--- Screens/Main/Profile.js
+++ Screens/Main/Profile.js
@@ -1,5 +1,11 @@
 import React, {Component} from 'react';
-import {SafeAreaView, StyleSheet, View, Image} from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
 import {
   Divider,
   Layout,
@@ -7,12 +13,16 @@ import {
   TopNavigation,
   TopNavigationAction,
   Icon,
+  Modal,
+  Button,
+  Card,
 } from '@ui-kitten/components';
 const LogoutIcon = props => <Icon {...props} name="log-out" />;
 
 export default class ProfileScreen extends Component {
   constructor(props) {
     super(props);
+    this.state = {visible: false, badgeText: ''};
   }
 
   BackAction = () => (
@@ -21,6 +31,33 @@ export default class ProfileScreen extends Component {
       onPress={() => this.props.route.params.mainFunctions.logout()}
     />
   );
+
+  ModalWithBackdropShowcase = (badgeString, imgUri) => {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity
+          onPress={() =>
+            this.setState({visible: true, badgeText: badgeString})
+          }>
+          <Image
+            style={{height: 100, width: 100, margin: 15}}
+            source={{
+              uri: imgUri,
+            }}
+          />
+        </TouchableOpacity>
+
+        <Modal
+          visible={this.state.visible}
+          backdropStyle={styles.backdrop}
+          onBackdropPress={() => this.setState({visible: false})}>
+          <Card disabled={true} style={{margin: 10}}>
+            <Text>{this.state.badgeText}</Text>
+          </Card>
+        </Modal>
+      </View>
+    );
+  };
 
   render() {
     return (
@@ -43,38 +80,69 @@ export default class ProfileScreen extends Component {
               />
             </View>
           </View>
-
           <View style={ProfileStyles.infoContainer}>
             <Text category={'h1'}>{global.realName}</Text>
             <Text category={'label'} appearance={'hint'}>
               {'@' + global.username}
             </Text>
           </View>
-          <View style={ProfileStyles.logoutContainer} />
+          <View style={ProfileStyles.logoutContainer}>
+            <Text category={'h3'}>Basarilarim</Text>
+            <View
+              style={{
+                height: '100%',
+                width: '100%',
+                flexDirection: 'row',
+                justifyContent: 'center',
+                marginTop: 20,
+              }}>
+              {this.ModalWithBackdropShowcase(
+                'Tebrikler! Tum aboneliklerin icin hatirlatici kurdun.',
+                'https://project-lyda.s3.eu-central-1.amazonaws.com/badges/clap.jpeg',
+              )}
+              {this.ModalWithBackdropShowcase(
+                'Tebrikler! Bir hafta boyunca arkadaslarindan daha tasarruflu davrandin.',
+                'https://project-lyda.s3.eu-central-1.amazonaws.com/badges/flag.jpg',
+              )}
+              {this.ModalWithBackdropShowcase(
+                'Tebrikler! Lyda hesabini bir banka hesabina bagladin.',
+                'https://project-lyda.s3.eu-central-1.amazonaws.com/badges/natural.jpeg',
+              )}
+            </View>
+          </View>
         </Layout>
       </SafeAreaView>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    minHeight: 192,
+  },
+  backdrop: {
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+});
+
 const ProfileStyles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    padding: 30,
   },
   avatarContainer: {
-    flex: 1,
+    flex: 2,
+    padding: 20,
   },
   infoContainer: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'flex-start',
     width: '100%',
+    padding: 20,
   },
   logoutContainer: {
-    flex: 1,
+    flex: 3,
     alignItems: 'center',
     justifyContent: 'flex-start',
     width: '100%',
@@ -85,12 +153,12 @@ const ProfileStyles = StyleSheet.create({
     resizeMode: 'contain',
     padding: 10,
     zIndex: 10,
-    borderRadius: 100
+    borderRadius: 1000,
   },
   avatarInnerContainer: {
-    height: 200,
-    width: 200,
-    borderRadius: 100,
+    height: '100%',
+    aspectRatio: 1,
+    borderRadius: 1000,
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: 'white',
