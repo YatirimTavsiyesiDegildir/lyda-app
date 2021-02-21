bb4f23473f1bec5470cbb6a5c132ec4403e67bdb
slariath <yunusck00@hotmail.com>
2021-02-21 20:35:48 +0300

    api init

--- index.js
+++ index.js
@@ -20,13 +20,32 @@ const client = new ApolloClient({
 client
   .query({
     query: gql`
-      query GetRates {
-        rates(currency: "USD") {
-          currency
+    query MyQuery {
+        users {
+          email
+          id
+          name
+          phone
+          tckn
+          username
         }
       }
     `
   })
-  .then(result => console.log(JSON.stringify(result.data.rates[0])));
+  .then(result => console.log(JSON.stringify(result)));
 
 AppRegistry.registerComponent(appName, () => App);
+
+
+var xhr = new XMLHttpRequest();
+var url = 'https://api.yapikredi.com.tr/api/creditcard/v1/creditCardTransactions';
+xhr.open('POST', url);
+xhr.setRequestHeader('Authorization', 'Bearer 7047fa49-dfce-4ee6-bdfa-293680097466');
+xhr.setRequestHeader('Content-Type', 'application/json');
+xhr.onreadystatechange = function () {
+    if (this.readyState == 4) {
+        //console.warn('Status: '+this.status+'\nHeaders: '+JSON.stringify(this.getAllResponseHeaders())+'\nBody: '+this.responseText);
+        console.warn(this.responseText[0]);
+    }
+};
+xhr.send('                    {  "request": {    "cardNo": "6353183025166336",    "cycle": "0"  }}                                      ');
