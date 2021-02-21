3eb8c236a28e7b82aa3c1968f5eea49b42f2dc7f
Dogu <dogudeniz.ugur@gmail.com>
2021-02-21 18:16:36 +0300

    graphql data got

--- index.js
+++ index.js
@@ -5,28 +5,48 @@ import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-import { gql } from '@apollo/client';
+import {ApolloClient, createHttpLink, InMemoryCache} from '@apollo/client';
+import {setContext} from '@apollo/client/link/context';
+import {gql} from '@apollo/client';
 
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: 'https://ypk.hasura.app/v1/graphql',
-  cache: new InMemoryCache()
 });
 
+const authLink = setContext((_, {headers}) => {
+  // get the authentication token from local storage if it exists
+  const token =
+    'E7v2cA2oCXG7xYcNaSeEFzOQo4p42UAGTdInXtmz4h7ZfS52XWmqKMzZq50AIEHD';
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      'content-type': 'application/json',
+      'x-hasura-admin-secret':
+        'E7v2cA2oCXG7xYcNaSeEFzOQo4p42UAGTdInXtmz4h7ZfS52XWmqKMzZq50AIEHD',
+    },
+  };
+});
 
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 // const client = ...
 
 client
   .query({
     query: gql`
-      query GetRates {
-        rates(currency: "USD") {
-          currency
+      query MyQuery {
+        users {
+          email
+          id
+          tckn
+          username
         }
       }
-    `
+    `,
   })
-  .then(result => console.log(JSON.stringify(result.data.rates[0])));
+  .then(result => console.warn(result));
 
 AppRegistry.registerComponent(appName, () => App);
