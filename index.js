ca19b1fe131e057e8c7552b66604bf5d4057d91a
slariath <yunusck00@hotmail.com>
2021-02-21 17:38:24 +0300

    Api info txt

--- index.js
+++ index.js
@@ -5,5 +5,28 @@ import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { gql } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache()
+});
+
+
+
+// const client = ...
+
+client
+  .query({
+    query: gql`
+      query GetRates {
+        rates(currency: "USD") {
+          currency
+        }
+      }
+    `
+  })
+  .then(result => console.log(JSON.stringify(result.data.rates[0])));
 
 AppRegistry.registerComponent(appName, () => App);
