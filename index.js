c1b590f1a09902f4be7f30a586c4e78154aa98a9
slariath <yunusck00@hotmail.com>
2021-02-21 17:47:49 +0300

    endpoint

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 import { gql } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  uri: 'https://ypk.hasura.app/v1/graphql',
   cache: new InMemoryCache()
 });
 
