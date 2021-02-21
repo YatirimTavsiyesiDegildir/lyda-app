a7b5361eaed1347a1b97eb4ccc65a24f8928ff01
Dogu <dogudeniz.ugur@gmail.com>
2021-02-21 18:19:54 +0300

    Refactoring

--- index.js
+++ index.js
@@ -8,29 +8,7 @@ import {name as appName} from './app.json';
 import {ApolloClient, createHttpLink, InMemoryCache} from '@apollo/client';
 import {setContext} from '@apollo/client/link/context';
 import {gql} from '@apollo/client';
-
-const httpLink = createHttpLink({
-  uri: 'https://ypk.hasura.app/v1/graphql',
-});
-
-const authLink = setContext((_, {headers}) => {
-  // get the authentication token from local storage if it exists
-  const token =
-    'E7v2cA2oCXG7xYcNaSeEFzOQo4p42UAGTdInXtmz4h7ZfS52XWmqKMzZq50AIEHD';
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      'content-type': 'application/json',
-      'x-hasura-admin-secret':
-        'E7v2cA2oCXG7xYcNaSeEFzOQo4p42UAGTdInXtmz4h7ZfS52XWmqKMzZq50AIEHD',
-    },
-  };
-});
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
+import {client} from './back-end/OurApi';
 
 // const client = ...
 
